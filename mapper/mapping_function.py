b67e276d7a778406c474b690f2d04c3540d97aef
Andreas Cola <andreas.cola@telicent.io>
2025-12-16 07:10:49 +0000

    feat: add ability to run map_func for local testing

--- mapper/mapping_function.py
+++ mapper/mapping_function.py
@@ -1,6 +1,6 @@
 import ies_tool.ies_tool as ies
 
-#add your mapping/enrichments/resolving in here
+# add your mapping/enrichments/resolving in here
 def map_func(item):
     """
     TODO: replace with logic associated to some sort of 
@@ -13,7 +13,9 @@ def map_func(item):
     mapped_item = item # currently feed back the source item as the mapped item
     return mapped_item
 
+# this section can be used to conduct local testing of the mapping_function by just running this file.
+if __name__ == "__main__":
 
-
-
-
+    test_data = ""
+    mapped_data = map_func(test_data)
+    print(mapped_data)
