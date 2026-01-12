1d111db274ef68786c52ee2e4236dfe6d27e389b
Helena <138475980+Helena-telicent@users.noreply.github.com>
2026-01-12 15:59:27 +0000

    fix:fix path

--- knowledge_mapper/mapping_function.py
+++ knowledge_mapper/mapping_function.py
@@ -45,6 +45,6 @@ def map_func(item):
 # this section can be used to conduct local testing of the mapping_function by just running this file.
 if __name__ == "__main__":
 
-    test_data = ""
+    test_data = 'data/sanctioned_individuals.csv'
     mapped_data = map_func(test_data)
     print(mapped_data)
